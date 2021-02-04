2934ea7b022501dbe5d57634d365a210ba04565b
RonaldoML <rmonserr@espol.edu.ec>
2021-02-04 10:22:24 -0500

    To deploy

--- utils/utils.js
+++ utils/utils.js
@@ -78,6 +78,6 @@ export const preRequest = {
         headers: {
           Authorization: token,
           'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': 'https://yelp-search-challenge.herokuapp.com',
+          'Access-Control-Allow-Origin': 'https://yelp-search-challenge.herokuapp.com/',
         }
 }
